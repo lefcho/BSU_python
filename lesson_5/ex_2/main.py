5e509f93a1297624bc23726d237d5982453e79e8
ivand <ivan.d.kostov@gmail.com>
2025-05-10 17:45:22 +0300

    added: files ex 2

--- lesson_5/ex_2/main.py
+++ lesson_5/ex_2/main.py
@@ -6,17 +6,17 @@ def double_integers():
         doubled_lines = []
         for line in lines:
             line = line.strip()
-            if line.isdigit() or (line.startswith('-') and line[1:].isdigit()):
+            if line.isdigit():
                 doubled_lines.append(str(int(line) * 2))
             else:
-                doubled_lines.append("Невалидни данни")
+                doubled_lines.append("Not an int")
 
         with open("doubled_ints.txt", "w", encoding="utf-8") as outfile:
             outfile.write("\n".join(doubled_lines))
 
         print("Числата са записани в doubled_ints.txt.")
     except FileNotFoundError:
-        print("Файлът int_data.txt не беше намерен.")
+        print("Error")
 
 
 double_integers()
